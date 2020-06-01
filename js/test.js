4d960aa24de9571e206e9dfad8030c67939738f5
annushka <anna.mitroshina@icloud.com>
2020-06-01 21:53:02 +0300

    init commit

---
+++ js/test.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+ }());
+
+let number = 5;
+// let - перменная изменяемая
+const leftBorderWidth = 1;
+
+
+number = 10;
+console.log(number);
+
+// использовать const хороший тон
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
+
+// прямых констант в ls нет
+
+
+
+var _name = 'Ivan'; 
+console.log(_name);
+// изменяемый тип данных
+
+{
+    let result = 20;
+}
+
+console.log(result);
